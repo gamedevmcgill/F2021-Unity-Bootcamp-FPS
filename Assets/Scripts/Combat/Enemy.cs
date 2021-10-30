61f5eb159d6e0d06a90322873faeb3815c3a4308
Feiyang Li <cred@feiyang.dev>
2021-10-30 17:26:18 -0400

    enemy patrol logical comments

--- Assets/Scripts/Combat/Enemy.cs
+++ Assets/Scripts/Combat/Enemy.cs
@@ -6,8 +6,48 @@ public class Enemy : MonoBehaviour {
 
     public NavMeshAgent NavAgent;
 
+    // create a member variable to keep track of the instructed destination
+
+    // create a member variable to keep track of the current patrolling state
+
+    // create a member variable that specifies instructed destination generation range
+
+    // create a member variable that specifies instructed destination proximity
+    // (how close to an instructed destination is considered "reaching the instructed destination"
+
+
     private void Start() {
         GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
         PlayerTransform = playerObject.transform;
+    }
+
+    private void FixedUpdate() {
+        // check to see if enemy is near the player
+
+
+            // if it is not, keep patrolling
+
+    }
+
+    private void Patrol() {
+        // check to see if there is an instructed destination assigned
+        // if it is, do nothing
+
+
+        // if it is not assigned, assign a new one
+
+
+            // randomly generate a point on the map
+
+
+            // ensure that the point is on the map
+
+
+            // if it is not, do nothing. Another attempt will be made in the next FixedUpdate()
+
+
+        // check if the enemy has reached an instructed destination (within a certain distance from the instructed destination)
+    
+
     }
 }
