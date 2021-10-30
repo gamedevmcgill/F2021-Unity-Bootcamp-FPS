6c600dab05cd688fb0e387ed068d04255fd6a367
Feiyang Li <cred@feiyang.dev>
2021-10-29 23:47:30 -0400

    Weapon.Shoot(): pos and dir

--- Assets/Scripts/Combat/Weapon.cs
+++ Assets/Scripts/Combat/Weapon.cs
@@ -4,6 +4,9 @@ public class Weapon : MonoBehaviour {
     public Transform EyeTransform;
 
     public void Shoot() {
+        Vector3 EyePosition = EyeTransform.position;
+        Vector3 EyeDirection = EyeTransform.forward;
+
 
     }
 }
