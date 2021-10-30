d21843caa4cf2c8df53c8eef8cc5c6e43d0a0e65
Feiyang Li <cred@feiyang.dev>
2021-10-30 00:19:05 -0400

    Weapon.Shoot(): debug draw ray

--- Assets/Scripts/Combat/Weapon.cs
+++ Assets/Scripts/Combat/Weapon.cs
@@ -7,6 +7,9 @@ public class Weapon : MonoBehaviour {
         Vector3 EyePosition = EyeTransform.position;
         Vector3 EyeDirection = EyeTransform.forward;
 
+        //          start position, ray direction,     color,    time
+        Debug.DrawRay(EyePosition, EyeDirection * 100, Color.red, 5.0f);
+
         RaycastHit hit;
         if (Physics.Raycast(EyePosition, EyeDirection, out hit)) {
             GameObject hitObject = hit.collider.gameObject;
